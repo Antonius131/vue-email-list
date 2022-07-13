8df9c146e5323533e5acdfb8bf0a620d488881ec
Antonius131 <antomendetta@gmail.com>
2022-07-13 17:06:25 +0200

    create list-group and .get random emails

--- js/script.js
+++ js/script.js
@@ -2,10 +2,17 @@ const app = new Vue (
    {
       el: '#root',
       data: {
-         
+         randomEmail: '',
       },
       methods: {
 
+      },
+      created() {
+         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+         .then((response) => {
+            const email = response.data.response;
+            this.randomEmail = email;
+         });
       }
    }
 )
